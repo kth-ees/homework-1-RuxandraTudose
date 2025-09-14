4828eb5b759afeb8a2173493b3bd2c78d23fb617
RuxandraTudose <ruxi.tudose@gmail.com>
2025-09-14 17:04:51 +0200

    q5

--- q5/bin2gray_tb.sv
+++ q5/bin2gray_tb.sv
@@ -6,5 +6,16 @@ module bin2gray_tb;
   // Outputs
   logic [3:0] gray;
 
-  // complete
+  bin2gray uut (
+    .binary(binary), 
+    .gray(gray)
+  );
+
+  initial begin 
+    for (int i = 0; i < 16; i++) begin
+      binary = i;
+      #20ns;
+    end 
+
+  end
 endmodule
